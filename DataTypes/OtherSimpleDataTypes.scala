2961ae9ffd9001a4d792bb7c5fdba381cd87405e
Francisco Charte Ojeda <francisco@fcharte.com>
2015-01-21 15:44:01 +0100

    Other data types example updated

--- DataTypes/OtherSimpleDataTypes.scala
+++ DataTypes/OtherSimpleDataTypes.scala
@@ -1,28 +1,33 @@
-println("\nBoolean values")
+println("\nBoolean values\n")
 val leapYear = false
-!leapYear // ! operator
+println("leapYear: " + leapYear)
+println("!leapYear: " + !leapYear) // ! operator
 
-println("\nAny type and null reference")
+println("\nAny type and null reference\n")
 var myObject: Any = null
-//myObject.getClass  // Produces NullPointerException
+println("Variable with null: " + myObject)
+println("Accessing null will throw an exception: " + myObject.getClass) // Produces NullPointerException
 
+println("Assigning values to Any variable")
 myObject = leapYear
-myObject.getClass  // MyObject is of Boolean class
+println("Current class: " + myObject.getClass)  // MyObject is of Boolean class
 
 myObject = leapYear.toString
-myObject.getClass  // MyObject is of String class
+println("Current class: " + myObject.getClass)  // MyObject is of String class
 
+println("\nAnyVal type\n")
 var myValue: AnyVal = 5
 myValue = 3.14
 myValue = false
 myValue = 1 to 5
 
+println("\nAnyRef type\n")
 var myReference: AnyRef = 1 to 5
 myReference = List(1,3,5)
 myReference = "A String"
 myReference = 5
 
-println("\nSymbols")
+println("\nSymbols\n")
 val symbol = 'trueorfalse
 symbol == true
 symbol == false
