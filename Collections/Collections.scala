5f5fb9f5027f62f384b4f3df8025d4a54294d9d7
Francisco Charte Ojeda <francisco@fcharte.com>
2015-02-06 09:57:10 +0100

    Started collections script

---
+++ Collections/Collections.scala
@@ -0,0 +1,30 @@
+
+// Scala collections come in three flavours, held in scala.collection.mutable, scala.collection.immutable and scala.collection.generic
+// By default immutable collection are used. If you need to use mutable ones, add 'import scala.collection.mutable' at the beginning
+
+import scala.collection.immutable._
+
+println("\nInitializing collections\n")
+
+println("A list is a sequential access collection")
+val aList = List(1, 2, 3, 5, 7, 11, 13)
+aList.getClass
+
+println("A vector is an indexed access collection")
+val aVector = Vector(1, 2, 3, 5, 7, 11, 13)
+
+println("A queue is a fifo data structure")
+val aQueue = Queue("first", "second", "third")
+
+println("A stack is a lifo data structure")
+val aStack = Stack("third", "second", "first")
+
+println("A map is an associative array")
+val aMap = Map("first" -> 1, "second" -> 2, "third" -> 3)
+
+println("The same operation applied to all collections")
+aList map println
+aVector map println
+aQueue map println
+aStack map println
+aMap map println
